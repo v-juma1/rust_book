24d87e98248e6669581a5f475fb58b501545d587
majun <majun@majundeMac-mini.lan>
2022-10-24 21:22:05 +0800

    update guessing_game

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 
 - **每个知识点对应一个binary crate,即src下的一个文件，用cargo workspace管理**
 - **用cargo run --bin 文件夹名称（即binary crate name）的方式运行**
+
+## 第2章
+
+- ​	添加crate 依赖：
+  - 在Cargo.toml文件[dependencies]下可按照如下的格式添加依赖
+  - rand = "0.7.0"
+  - 上面的格式其实是rand = "^0.7.0"的简写，表示依赖在0.7.0以上，但小于0.8.0的版本，cargo build 第一次运行时会创建Cargo.lock文件，并将依赖版本写入，只有在Cargo.toml更新了0.8.0以上的版本时，Cargo.lock文件才会更新
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,34 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
 fn main() {
-    println!("Hello, world!");
+    println!("猜数字游戏!");
+
+    let secret_num = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("请输入一个数字!");
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("读取失败!");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(n) => n,
+            Err(_) => {
+                println!("你输入的数字不正确，请重新输入");
+                continue;
+            }
+        };
+
+        println!("你猜的数字：{guess}");
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("太小了......"),
+            Ordering::Greater => println!("太大了......"),
+            Ordering::Equal => {
+                println!("猜中了");
+                break;
+            }
+        }
+    }
 }
